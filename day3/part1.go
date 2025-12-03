712a0fdeaaf31e788c05b2949745d4797f2119c5
madsken <gamercoke@gmail.com>
2025-12-03 18:16:25 +0100

    day 3

--- day3/part1.go
+++ day3/part1.go
@@ -2,20 +2,54 @@ package main
 
 import (
 	"fmt"
-	//"log"
-	//"os"
-	//"strconv"
-	//"strings"
+	"log"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func Part1(file string) {
-	//fileBytes, err := os.ReadFile(file)
-	//if err != nil {
-	//log.Fatal("Error reding file")
-	//}
-	//fileString := string(fileBytes)
+	fileBytes, err := os.ReadFile(file)
+	if err != nil {
+		log.Fatal("Error reding file")
+	}
+	fileString := string(fileBytes)
 
 	result := 0
+
+	for bank := range strings.SplitSeq(fileString, "\n") {
+		if bank == "" {
+			continue
+		}
+		largestSoFar := 0
+		largestIdx := 0
+
+		for idx, charge := range bank[:len(bank)-1] {
+			cNumber, err := strconv.Atoi(string(charge))
+			if err != nil {
+				panic(err)
+			}
+
+			if cNumber > largestSoFar {
+				largestSoFar = cNumber
+				largestIdx = idx
+			}
+		}
+
+		largestSoFar2 := 0
+		for _, charge := range bank[largestIdx+1:] {
+			cNumber, err := strconv.Atoi(string(charge))
+			if err != nil {
+				panic(err)
+			}
+
+			if cNumber > largestSoFar2 {
+				largestSoFar2 = cNumber
+			}
+		}
+
+		result += (largestSoFar * 10) + largestSoFar2
+	}
 
 	fmt.Println(result)
 }
--- day3/part2.go
+++ day3/part2.go
@@ -2,20 +2,48 @@ package main
 
 import (
 	"fmt"
-	//"log"
-	//"os"
-	//"strconv"
-	//"strings"
+	"log"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func Part2(file string) {
-	//fileBytes, err := os.ReadFile(file)
-	//if err != nil {
-	//log.Fatal("Error reding file")
-	//}
-	//fileString := string(fileBytes)
-
+	fileBytes, err := os.ReadFile(file)
+	if err != nil {
+		log.Fatal("Error reding file")
+	}
+	fileString := string(fileBytes)
 	result := 0
 
+	for bank := range strings.SplitSeq(fileString, "\n") {
+		if bank == "" {
+			continue
+		}
+		number, err := strconv.Atoi(GetLargest12DigitNumber(string(bank)))
+		if err != nil {
+			panic(err)
+		}
+		result += number
+	}
+
 	fmt.Println(result)
+}
+
+func GetLargest12DigitNumber(number string) string {
+	digitsToRemove := len(number) - 12
+	largestNumber := make([]byte, 0, 12)
+
+	for i := 0; i < len(number); i++ {
+		digit := number[i]
+
+		for digitsToRemove > 0 && len(largestNumber) > 0 && largestNumber[len(largestNumber)-1] < digit {
+			largestNumber = largestNumber[:len(largestNumber)-1]
+			digitsToRemove -= 1
+		}
+
+		largestNumber = append(largestNumber, digit)
+	}
+
+	return string(largestNumber[:12])
 }
