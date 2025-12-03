36b83cb3398e7c741acb71d07358d55f656e0f38
madsken <gamercoke@gmail.com>
2025-12-03 16:39:32 +0100

    deleted input.txt from older days

---
+++ day3/go.mod
@@ -0,0 +1,3 @@
+module github.com/madsken/aoc-2025/day3
+
+go 1.25.4
---
+++ day3/part2.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	//"log"
+	//"os"
+	//"strconv"
+	//"strings"
+)
+
+func Part2(file string) {
+	//fileBytes, err := os.ReadFile(file)
+	//if err != nil {
+	//log.Fatal("Error reding file")
+	//}
+	//fileString := string(fileBytes)
+
+	result := 0
+
+	fmt.Println(result)
+}
